b0f6ceb1035a19642a81d2dc2b10689ebc071111
Khanh <teteka911@gmail.com>
2024-01-18 18:40:48 +0700

    update prisma

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
+import rootRoutes from "./src/routes/rootRoutes";
 
 const app = express();
 const port = 8082;
 
-app.use(express.json());
-app.use(express.static("."));
-
+app.use(express.json()); // middleware parse body string => body json
+app.use(express.static(".")); // middleware để xác định nơi lưu file
+app.use(cors()); // cho tất cả các request (FE) từ bên ngoài vào để tương tác vs BE
+app.use(rootRoutes);
 app.get("/", (req, res) => {
-  res.send("Hello node38 youtube");
+  res.send("Capstone");
 });
 
 app.listen(port, () => {
